0634395b380e1c50749ff00003bcee802d80c214
Dipen Khadka <dipenkhadka191@gmail.com>
2021-06-20 11:34:38 +0545

    View Branch Created - Layouts Created - Added CSS,Footer,Header and Navbar - Added Bootstrap CSS(Inside Assets Folder)

---
+++ application/views/layout/navbar.php
@@ -0,0 +1,16 @@
+<nav class="navbar  navbar-dark bg-dark navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="/ci">CRUD</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+            <a class="nav-link" href="insert">Insert</a>
+            <a class="nav-link" href="view">View</a>
+            <a class="nav-link" href="#">Update</a>
+            <a class="nav-link" href="#">Delete</a>
+        </div>
+        </div>
+    </div>
+</nav>
